f24c9761f1a763aab440db9a03418ee736a0e830
Maggie <maggiejgray@gmail.com>
2019-04-12 12:06:01 -0500

    dasdfasdf

--- src/js/components/container/app.jsx
+++ src/js/components/container/app.jsx
@@ -38,16 +38,13 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.updateCurrentProduct();
-    // this.setState ({
-    //   currentImage: this.state.currentProduct.photo_url
-    // })
+    window.addEventListener('changeItem', (event)=>{this.updateCurrentProduct(event.detail)})
   }
 
-  updateCurrentProduct() {
+  updateCurrentProduct(sku) {
     axios.get('http://ec2-3-16-128-154.us-east-2.compute.amazonaws.com:3003/product_info', {
       params: {
-        sku: window.State || Math.floor(Math.random() * 100) + 1
+        sku: sku || Math.floor(Math.random() * 100) + 1
       }
     })
     .then((response) => {
